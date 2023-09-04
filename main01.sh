87c2fb9b9fd59e873b03138d44547169db532c61
YMXxtc <57615087+YMXxtc@users.noreply.github.com>
2023-09-04 16:22:14 +0800

    Update main01.sh

--- main01.sh
+++ main01.sh
@@ -5,4 +5,4 @@ python deep_steg.py -g 0,1,2,3
 python stegan_cuda.py --netG Res/netG_epoch_deep_steg_72.pth -g 0 --config deep_steg
 
 #STC embedding
-matlab -nosplash -nodesktop -r "embedding /data/ymx/ymx/deep_steg/deep_steg_unet;exit" 
+matlab -nosplash -nodesktop -r "embedding /data/ymx/ymx/deep_steg/deep_steg;exit" 
